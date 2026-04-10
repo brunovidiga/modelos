fefc5cfbf8b008f646c354242f758700924c2ed7
Bruno Vidiga <brunovidiga@github.com>
2026-04-10 10:35:11 -0300

    Initial commit - Animale project

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        v2: resolve(__dirname, 'v2/index.html'),
+        v3: resolve(__dirname, 'v3/index.html')
+      }
+    }
+  }
+});
